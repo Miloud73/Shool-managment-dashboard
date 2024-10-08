e7d3510e19d9bbce826481359b60c166b6bc45d4
Miloud Bendjedda <111144391+Miloud73@users.noreply.github.com>
2024-10-08 18:35:11 +0200

    implement userCard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import UserCard from "@/components/UserCard";
 import type { Metadata } from "next";
 import React from "react";
 export const metadata: Metadata = {
@@ -7,7 +8,18 @@ export const metadata: Metadata = {
 
 const AdminPage = () => {
   return (
-    <div>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFt SIDE */}
+      <div className="w-full lg:w-2/3 ">
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student"/>
+          <UserCard type="admin"/>
+          <UserCard type="teacher"/>
+          <UserCard type="staff"/>
+        </div>
+      </div>
+      {/* RIGHT SIDE */}
+      <div className="w-full lg:w-1/3 ">RIGHT</div>
      
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import Image from "next/image";
-import Menu from "@/components/Menu";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "School Management Dashboard",
-  description: "School Management System",
+  title: " Dev School Management Dashboard",
+  description: " School Management System",
 };
 
 export default function RootLayout({
@@ -20,23 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="relative flex" >
-          {/* left */}
-          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 h-screen">
-            <Link href="/" className="flex item-center justify-content lg:justify-start gap-2">
-              <Image src="/logo.png" alt="logo" width={32} height={32}/>
-              <span className="hidden lg:block mt-2 ">UDL-univ</span>
-            </Link>
-            <Menu/>
-          </div>
-          {/* right */}
-          <div className="w-[86%] md:w-[92%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
-            <Navbar/>  
-          </div>
-        </div>
-        {children}
-        </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
